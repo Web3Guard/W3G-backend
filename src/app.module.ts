e78d8d093a442d02f09ea2e35345c1ea10b8fc90
Biswajit Patra <patrabiswajit133@gmail.com>
2022-11-11 01:05:06 +0530

    Add pine logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { RatingModule } from './rating/rating.module';
 import { InstantModule } from './instant/instant.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
-    RatingModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: (req, res) => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+        },
+      }
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
+
     InstantModule,
+    RatingModule
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
   app.enableCors({
     origin: true,
     methods: '*',
     allowedHeaders: '*',
     credentials: true,
   });
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
   await app.listen(3001);
 }
 bootstrap();
