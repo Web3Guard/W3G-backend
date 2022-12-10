879ae1593133ef03359340ce0587caa924ce59f3
Rahul Mishra <rahulmishra24>
2022-12-10 13:11:42 +0530

    Modified wallet_address_strategy

--- src/auth/wallet_address.strategy.ts
+++ src/auth/wallet_address.strategy.ts
@@ -1,25 +1,40 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-custom";
-import { PrismaService } from "src/utils/prisma.service";
-import { SupabaseService } from "src/utils/supabase.service";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-custom';
+import { PrismaService } from 'src/utils/prisma.service';
+import { SupabaseService } from 'src/utils/supabase.service';
 
 @Injectable()
-export class WalletAddressStrategy extends PassportStrategy(Strategy, "wallet_address") {
-  constructor(private prisma: PrismaService, private supabase: SupabaseService) {
+export class WalletAddressStrategy extends PassportStrategy(
+  Strategy,
+  'wallet_address',
+) {
+  constructor(
+    private prisma: PrismaService,
+    private supabase: SupabaseService,
+  ) {
     super();
   }
 
   async validate(req: Request) {
     const wallet = await this.prisma.wallets.findUnique({
-      //TODO: get wallet address and chain id from requset 
-      where: { address_chain_id: { 
-        address: req.json()["address"],
-        chain_id: req.json()["chain_id"]
-      }}
-    })
+      //TODO: get wallet address and chain id from requset
+      where: {
+        address_chain_id: {
+          address: req.body['from'],
+          chain_id: req.body['network_id'],
+        },
+      },
+    });
 
-    if(!wallet) throw new UnauthorizedException("Wallet address not registered with any user");
-    return (await this.supabase.clientInstance.auth.admin.getUserById(wallet.user_id)).data.user;
+    console.log('wallet', wallet);
+
+    if (!wallet)
+      throw new UnauthorizedException(
+        'Wallet address not registered with any user',
+      );
+    return (
+      await this.supabase.clientInstance.auth.admin.getUserById(wallet.user_id)
+    ).data.user;
   }
 }
